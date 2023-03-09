78794473ab2d0785a84ed69e269697436dd344d4
Azam <gulovazam@gmail.com>
2023-03-09 18:17:28 +0500

    Initial commit

---
+++ src/modules/taskController.js
@@ -0,0 +1,31 @@
+import { isSameDay, isSameWeek } from "date-fns";
+import Projects from "./projects";
+
+export default class taskController {
+  static getAllTasks() {
+    return Projects.list.reduce((tasks, project) => {
+      tasks.push(project.tasks);
+      return tasks.flat();
+    }, []);
+  }
+
+  static getCompletedTasks() {
+    return this.getAllTasks().filter((task) => task.status === "checked");
+  }
+
+  static getTodaysTasks() {
+    const dateToCompare = new Date();
+    const todaysTask = this.getAllTasks().filter((task) =>
+      isSameDay(dateToCompare, new Date(task.dueDate))
+    );
+    return todaysTask;
+  }
+
+  static getThisWeeksTasks() {
+    const dateToCompare = new Date();
+    const thisWeeksTasks = this.getAllTasks().filter((task) =>
+      isSameWeek(dateToCompare, new Date(task.dueDate))
+    );
+    return thisWeeksTasks;
+  }
+}
